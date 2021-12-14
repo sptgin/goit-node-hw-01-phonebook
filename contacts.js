41e1c4868f64991b45dd92b662c659579a471aea
Sergej Pjatygin <sergej.pjatygin@gmail.com>
2021-12-15 01:40:58 +0200

    initial commit

---
+++ contacts.js
@@ -0,0 +1,51 @@
+const fs = require('fs/promises');
+const path = require('path');
+const crypto = require('crypto');
+const uuid = require('uuid');
+
+const readContent = async () => {
+  const content = await fs.readFile(
+    path.join(__dirname, 'db', 'contacts.json'),
+    'utf8',
+  );
+  const result = JSON.parse(content);
+  return result;
+};
+
+const listContacts = async () => {
+  return await readContent();
+};
+
+const getContactById = async contactId => {
+  const contacts = await readContent();
+  const [contact] = contacts.filter(contact => contact.id === contactId);
+  return contact;
+};
+
+const removeContact = async contactId => {
+  const contacts = await readContent();
+  const contactsForRemove = contacts.find(contact => contact.id == contactId);
+  if (contactsForRemove) {
+    console.log('ContactForRemove +');
+    return;
+  }
+  console.log('ContactForRemove - ');
+  // await fs.writeFile(
+  //   path.join(__dirname, 'db', 'contacts.json'),
+  //   JSON.stringify(contacts, null, 2),
+  // );
+  // return newContact;
+};
+
+const addContact = async (name, email, phone) => {
+  const contacts = await readContent();
+  const newContact = { name, email, phone, id: crypto.randomUUID() };
+  contacts.push(newContact);
+  await fs.writeFile(
+    path.join(__dirname, 'db', 'contacts.json'),
+    JSON.stringify(contacts, null, 2),
+  );
+  return newContact;
+};
+
+module.exports = { listContacts, getContactById, removeContact, addContact };
